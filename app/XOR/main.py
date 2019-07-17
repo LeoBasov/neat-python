d51e782b7487ff95bbc44635ef471f894682d632
leo <basov.leo@googlemail.com>
2019-07-17 17:21:26 +0200

    Added stuff to xor application.

--- app/XOR/main.py
+++ app/XOR/main.py
@@ -24,8 +24,10 @@ class XORNetwork(Network):
 
 	def _set_up_genes(self):
 		gene1 = Gene(in_node = 0, out_node = 3, weight = 1.0, enabled = True)
+		gene2 = Gene(in_node = 1, out_node = 3, weight = 1.0, enabled = True)
+		gene3 = Gene(in_node = 2, out_node = 3, weight = 1.0, enabled = True)
 
-		self.set_genes([gene1])
+		self.set_genes([gene1, gene2, gene3])
 
 def generate_networks(number):
 	networks = []
@@ -61,10 +63,8 @@ def mutate(evaluated_networks):
 	new_networks = len(evaluated_networks)*[None]
 
 	for i in range(len(evaluated_networks)):
-		if evaluated_networks[i][0] < 0.8:
-			new_networks[i] = neat.mutate(evaluated_networks[i][1])
-		else:
-			new_networks[i] = evaluated_networks[i][1]
+		new_networks[i] = neat.mutate(evaluated_networks[i][1])
+		new_networks[len(evaluated_networks) - 1 - i] = neat.mutate(evaluated_networks[i][1])
 
 	return new_networks
 
@@ -72,8 +72,8 @@ def sigmoid(x):
 	return 1 / (1 + math.exp(-x))
 
 def main():
-	number_networks = 1
-	number_itterations = 100
+	number_networks = 100
+	number_itterations = 1000
 	networks = generate_networks(number_networks)
 
 	print("Evaluating networks")
@@ -86,6 +86,8 @@ def main():
 
 	print("")
 	print_best(lis)
+	print(80*"-")
+	test_best(lis[0][1])
 	print("done")
 
 def print_best(lis):
@@ -93,6 +95,20 @@ def print_best(lis):
 
 	for key, node in lis[0][1].nodes.items():
 		print(node)
+
+def test_best(network):
+	for i in range(10):
+		val1 = round(random.random())
+		val2 = round(random.random())
+
+		input_values_node_ids = [(val1, 1), (val2, 2)]
+
+		network.execute(input_values_node_ids)
+
+		value = sigmoid(network.nodes[3].value)
+		fitness = 1.0 - abs(float(val1 != val2) - sigmoid(network.nodes[3].value))
+
+		print("Value 1: {}, Value 2: {}, XOR: {}, Network: {}, Fitness: {}".format(val1, val2, float(val1 != val2), value, fitness))
 
 if __name__ == '__main__':
 	main()
