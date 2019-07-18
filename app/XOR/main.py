589cde9005fcf0443ddf272758241bbc35d82fce
leo <basov.leo@googlemail.com>
2019-07-18 11:13:31 +0200

    Changed evaluation process.

--- app/XOR/main.py
+++ app/XOR/main.py
@@ -23,11 +23,20 @@ class XORNetwork(Network):
 		self._set_up_genes()
 
 	def _set_up_genes(self):
-		gene1 = Gene(in_node = 0, out_node = 3, weight = 1.0, enabled = True)
-		gene2 = Gene(in_node = 1, out_node = 3, weight = 1.0, enabled = True)
-		gene3 = Gene(in_node = 2, out_node = 3, weight = 1.0, enabled = True)
+		gene1 = Gene(in_node = 0, out_node = 4, weight = 10 - 20.0*random.random(), enabled = True)
+		gene2 = Gene(in_node = 0, out_node = 5, weight = 10 - 20.0*random.random(), enabled = True)
+		gene3 = Gene(in_node = 0, out_node = 3, weight = 10 - 20.0*random.random(), enabled = True)
 
-		self.set_genes([gene1, gene2, gene3])
+		gene4 = Gene(in_node = 1, out_node = 4, weight = 10 - 20.0*random.random(), enabled = True)
+		gene5 = Gene(in_node = 1, out_node = 5, weight = 10 - 20.0*random.random(), enabled = True)
+
+		gene6 = Gene(in_node = 2, out_node = 4, weight = 10 - 20.0*random.random(), enabled = True)
+		gene7 = Gene(in_node = 2, out_node = 5, weight = 10 - 20.0*random.random(), enabled = True)
+
+		gene8 = Gene(in_node = 4, out_node = 3, weight = 10 - 20.0*random.random(), enabled = True)
+		gene9 = Gene(in_node = 5, out_node = 3, weight = 10 - 20.0*random.random(), enabled = True)
+
+		self.set_genes([gene1, gene2, gene3, gene4, gene5, gene6, gene7, gene8, gene9])
 
 def generate_networks(number):
 	networks = []
@@ -42,16 +51,20 @@ def evaluate(networks):
 	lis = []
 
 	for network in networks:
-		val1 = round(random.random())
-		val2 = round(random.random())
+		fitness = 0
 
-		input_values_node_ids = [(val1, 1), (val2, 2)]
+		for i in range(100):
+			val1 = round(random.random())
+			val2 = round(random.random())
 
-		network.execute(input_values_node_ids)
+			input_values_node_ids = [(val1, 1), (val2, 2)]
 
-		fitness = 1.0 - abs(float(val1 != val2) - sigmoid(network.nodes[3].value))
+			network.execute(input_values_node_ids)
 
-		lis.append((fitness, network))
+			fitness += 1.0 - abs(float(val1 != val2) - sigmoid(network.nodes[3].value))
+
+
+		lis.append((fitness/100, network))
 
 	lis = sorted(lis, key = lambda x: x[0]) 
 	lis.reverse()
@@ -63,8 +76,12 @@ def mutate(evaluated_networks):
 	new_networks = len(evaluated_networks)*[None]
 
 	for i in range(len(evaluated_networks)):
-		new_networks[i] = neat.mutate(evaluated_networks[i][1])
-		new_networks[len(evaluated_networks) - 1 - i] = neat.mutate(evaluated_networks[i][1])
+		if i < 0.2*len(evaluated_networks):
+			new_networks[i] = evaluated_networks[i][1]
+		elif i < 0.6*len(evaluated_networks):
+			new_networks[i] = neat.mutate(evaluated_networks[i][1])
+		else:
+			new_networks[i] = XORNetwork()
 
 	return new_networks
 
@@ -72,8 +89,8 @@ def sigmoid(x):
 	return 1 / (1 + math.exp(-x))
 
 def main():
-	number_networks = 100
-	number_itterations = 1000
+	number_networks = 10
+	number_itterations = 10000
 	networks = generate_networks(number_networks)
 
 	print("Evaluating networks")
