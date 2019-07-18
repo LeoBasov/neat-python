c3a7fd921b7ebc15e143891a630f0f9ae2c375c5
leo <basov.leo@googlemail.com>
2019-07-18 15:30:00 +0200

    Modified utility.sigmoid function.

--- neat/utility.py
+++ neat/utility.py
@@ -17,4 +17,10 @@ long with this program. If not, see <https://www.gnu.org/licenses/>."""
 import math
 
 def sigmoid(value):
-	return 1.0/(1.0 + math.exp(-value))
+	try:
+		return 1.0/(1.0 + math.exp(-value))
+	except OverflowError:
+		if value > 0:
+			return 1.0
+		else:
+			return 0.0
--- test/utility_test.py
+++ test/utility_test.py
@@ -7,10 +7,10 @@ import neat.utility as utility
 
 class UtilityTest(unittest.TestCase):
 	def test_sigmoid(self):
-		value_min = -100.0
-		value_max =  100.0
+		value_min = -sys.float_info.max
+		value_max =  sys.float_info.max
 		value_mean = 0.0
 
-		self.assertTrue(utility.sigmoid(value_min) < 1.0e-10)
+		self.assertTrue(utility.sigmoid(value_min) == 0.0)
 		self.assertEqual(utility.sigmoid(value_mean), 0.5)
-		self.assertTrue(utility.sigmoid(value_max) > 0.9999999)
+		self.assertTrue(utility.sigmoid(value_max) == 1.0)
