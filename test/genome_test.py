8ef31016a695a6146710fcf64647372267028ce2
leo <basov.leo@googlemail.com>
2019-07-23 13:48:20 +0200

    Fixed bug in node.

--- test/genome_test.py
+++ test/genome_test.py
@@ -8,6 +8,7 @@ from neat.genome import HiddenNode
 from neat.genome import NodeType as Type
 from neat.genome import Error
 from neat.genome import Gene
+from neat.genome import Genome
 
 class NodeTest(unittest.TestCase):
 	def test_init(self):
@@ -33,3 +34,37 @@ class GeneTest(unittest.TestCase):
 
 		self.assertFalse(gene.connection_exists(1, 1))
 		self.assertFalse(gene.connection_exists(0, 0))
+
+class GenomeTest(unittest.TestCase):
+	def test_add_node(self):
+		genome = Genome()
+
+		input_node_id_1 = genome.add_input_node()
+		input_node_id_2 = genome.add_input_node()
+
+		hidden_node_id_1 = genome.add_hidden_node()
+		hidden_node_id_2 = genome.add_hidden_node()
+		hidden_node_id_3 = genome.add_hidden_node()
+
+		output_node_id_1 = genome.add_output_node()
+		output_node_id_2 = genome.add_output_node()
+
+		self.assertTrue(input_node_id_1 in range(len(genome.nodes)))
+		self.assertTrue(input_node_id_2 in range(len(genome.nodes)))
+
+		self.assertTrue(hidden_node_id_1 in range(len(genome.nodes)))
+		self.assertTrue(hidden_node_id_2 in range(len(genome.nodes)))
+		self.assertTrue(hidden_node_id_3 in range(len(genome.nodes)))
+
+		self.assertTrue(output_node_id_1 in range(len(genome.nodes)))
+		self.assertTrue(output_node_id_2 in range(len(genome.nodes)))
+
+		self.assertFalse(input_node_id_1 in genome.output_nodes_ids)
+		self.assertFalse(input_node_id_2 in genome.output_nodes_ids)
+
+		self.assertFalse(hidden_node_id_1 in genome.output_nodes_ids)
+		self.assertFalse(hidden_node_id_2 in genome.output_nodes_ids)
+		self.assertFalse(hidden_node_id_3 in genome.output_nodes_ids)
+
+		self.assertTrue(output_node_id_1 in genome.output_nodes_ids)
+		self.assertTrue(output_node_id_2 in genome.output_nodes_ids)
