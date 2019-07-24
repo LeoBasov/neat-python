6e8656f3f7284fdcfab54d776d7ada3240bfd598
Leo Basov <basov.leo@gmail.com>
2019-07-24 11:19:28 +0200

    Added neat_slim

---
+++ neat/neat_slim.py
@@ -0,0 +1,42 @@
+"""NEAT
+Copyright (C) 2019  Leo Basov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+long with this program. If not, see <https://www.gnu.org/licenses/>."""
+
+import copy
+import random
+
+from .network_slime import Node
+from .network_slime import Network
+from .network_slime import Gene
+from .network_slime import Genenome
+
+
+class NEAT:
+	def __init__(self):
+		self.weight_variance = 10.0
+
+	def mutate(self, network):
+		genome = copy.deepcopy(Node.genome)
+
+		#mutate genome
+
+		return Network(genome)
+
+	def add_new_connection(self, genome):
+		node1 = random.choice(genome.nodes)
+		node2 = random.choice(genome.nodes)
+		weight = self.weight_variance - 2.0*self.weight_variance*random.random()
+
+		genome.add_new_connection(node1.id, node2.id, weight) 
