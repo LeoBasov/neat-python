d9bebb4f1bf982e7d0a5c3c37ea923c0b83efe47
leo <basov.leo@googlemail.com>
2019-08-12 16:05:55 +0200

    Changed order in main.

--- app/LIN_INTERPOL/main.py
+++ app/LIN_INTERPOL/main.py
@@ -11,11 +11,11 @@ def main():
 	parameters["number_networks"] = 150
 	parameters["number_itterations"] = 1
 	parameters["number_sub_cycles"] = 1
-	parameters['number_output_nodes'] = 4
 
 	parameters["test_case_name"] = "XOR NETWORK EVOLUTION"
 	parameters["test_case_specifics"] = ["Evolution of XOR network starting with minimal configuration", "Two input and one output node"]
 
+	parameters['number_output_nodes'] = 4
 	parameters["number_hidden_nodes"] = 1
 	parameters["number_genes"] = 4
 
