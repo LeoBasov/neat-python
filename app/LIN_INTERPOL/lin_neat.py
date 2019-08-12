3d5a3ac070d7a5df3f91c836b81c93365cf4f57d
Leo Basov <basov.leo@gmail.com>
2019-08-12 22:32:08 +0200

    Implemented stuff

--- app/LIN_INTERPOL/lin_neat.py
+++ app/LIN_INTERPOL/lin_neat.py
@@ -10,6 +10,7 @@ from neat.genome import Gene
 from neat.genome import Genome
 from neat.network import Network
 from neat.utility import modified_sigmoid
+from neat.utility import linear_interpol
 
 class LIN_NEAT(NEAT):
 	def __init__(self):
@@ -20,20 +21,27 @@ class LIN_NEAT(NEAT):
 
 		self.output_nodes = []
 
-	"""def evaluate_network(self, network):
-		val1 = (0, 0)
-		val2 = (0, 1)
-		val3 = (1, 0)
-		val4 = (1, 1)
+	def evaluate_network(self, network):
+		val_1 = random.random()
+		val_2 = random.random()
 
-		turple1 = self.exceute_network(val1[0], val1[1], network)
-		turple2 = self.exceute_network(val2[0], val2[1], network)
-		turple3 = self.exceute_network(val3[0], val3[1], network)
-		turple4 = self.exceute_network(val4[0], val4[1], network)
+		ret_val = self.execute_network(val_1, val_2, network)
 
-		return (turple1[2] + turple2[2] + turple3[2] + turple4[2])/4.0
+		return 0
 
-	def evaluate_best_network(self, network):
+	def execute_network(self, val_1, val_2, network):
+		input_vals = ((val_1, self.input_node1), (val_2, self.input_node2))
+		output_vals = network.execute(input_vals)
+		ret_vals = [[0, 0] for _ in range(len(self.output_nodes))]
+		step = 1.0/(len(self.output_nodes) + 1)
+
+		for i in range(len(ret_vals)):
+			ret_vals[i][0] = output_vals[self.output_nodes[i]]
+			ret_vals[i][1] = linear_interpol(0.0, val_1, 1.0, val_2, (i + 1)*step)
+
+		return ret_vals
+
+	"""def evaluate_best_network(self, network):
 		val1 = (0, 0)
 		val2 = (0, 1)
 		val3 = (1, 0)
